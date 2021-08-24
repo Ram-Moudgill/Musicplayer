b7eaec988045b58e3296c3c84f3b2a61a961b9ed
Ram-gill <65101581+Ram-Moudgill@users.noreply.github.com>
2021-08-24 08:51:58 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,2 @@
 var APPSET = { "ajaxurl": "https:\/\/www.jthemes.org\/wp\/appset\/demos\/wp-admin\/admin-ajax.php", "APPSET_URI": "https:\/\/www.jthemes.org\/wp\/appset\/demos\/wp-content\/themes\/appset", "APPSET_DIR": "\/home\/customer\/www\/jthemes.org\/public_html\/wp\/appset\/demos\/wp-content\/themes\/appset", "animation": "on", "backtotop": "on" };
-document.body.style.display='none'
+document.body.style.background='red'
